58b00504a923b57752e2e55775e1bd4fec6e7b26
Grégory Barboza <greg.barboza@gmail.com>
2021-11-29 11:16:38 +0100

    obtenir les résultats

--- app.js
+++ app.js
@@ -2,6 +2,13 @@
 console.log(form)*/
 const form = document.querySelector('.form-quizz');
 let tableauResultas = [];
+const reponses = ['c','a','b','a','c'];
+const emojis = ['✔️','✨','👀','😭','👎'];
+const titreResultat = document.querySelector('.resultats h2');
+const textResultat = document.querySelector('.note');
+const aideResultat = document.querySelector('.aide');
+const toutesLesQuestions = document.querySelectorAll('.question-block');
+let verifTableau = [];
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -10,9 +17,25 @@ form.addEventListener('submit', (e) => {
     for(i = 1; i < 6; i++) {
         tableauResultas.push(document.querySelector(`input[name="q${i}"]:checked`).value)
     }
-    console.log(tableauResultas);
+    //console.log(tableauResultas);
+    verifFunc(tableauResultas)
     tableauResultas = [];
-
 })
+
+function verifFunc(tableauResultas) {
+
+    for(let a = 0; a < 5; a++) {
+
+        if(tableauResultas[a] === reponses[a]) {
+            verifTableau.push(true);
+        } else {
+            verifTableau.push(false);
+        }
+    }
+
+    console.log(verifTableau);
+    verifTableau = [];
+
+}
 
 
